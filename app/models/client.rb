e921b2594af83df0cba2db332ca2c1b11f4edc3a
Mateusz Pietrzyk <protoplaste@gmail.com>
2022-09-13 01:51:10 +0200

    add reservations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,4 @@
 class Client < ApplicationRecord
   has_one :profile
+  has_many :reservations
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_234648) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_234924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,5 +31,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_234648) do
     t.index ["client_id"], name: "index_profiles_on_client_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "client_id", null: false
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_reservations_on_client_id"
+  end
+
   add_foreign_key "profiles", "clients"
+  add_foreign_key "reservations", "clients"
 end
